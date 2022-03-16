cf07ecd6a1cd9ad1c73fbedd678f477e5d487cd7
AdamaCoulibaly <ce.adamacoulibaly@gmail.com>
2022-03-16 08:30:08 -0600

    Initial commit

---
+++ app/src/main/java/com/revature/listview/Plant.kt
@@ -0,0 +1,8 @@
+package com.revature.listview
+
+data class Plant(
+    val id:Int,
+    val name:String,
+    val description:String,
+    val imgaeRes:Int
+)
---
+++ app/src/main/java/com/revature/listview/Plants.kt
@@ -0,0 +1,4 @@
+package com.revature.listview
+
+public class Plants {
+}
